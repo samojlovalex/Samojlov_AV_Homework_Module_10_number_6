39b5159c0d4732bca4b94d36184aa3148c7f5361
Alexandr Samojlov <samojlovalex@gmail.com>
2024-08-07 17:48:34 +0500

    Initial commit

---
+++ app/src/main/java/com/example/samojlov_av_homework_module_10_number_6/BodyMassIndexActivity.kt
@@ -0,0 +1,80 @@
+package com.example.samojlov_av_homework_module_10_number_6
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.samojlov_av_homework_module_10_number_6.databinding.ActivityBodyMassIndexBinding
+
+
+class BodyMassIndexActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityBodyMassIndexBinding
+    private lateinit var toolbarBodyMassIndex: androidx.appcompat.widget.Toolbar
+    private lateinit var textViewBodyMassIndexTV: TextView
+    private lateinit var imageViewIV: ImageView
+    private lateinit var textViewRecommendationsTV: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityBodyMassIndexBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+//        setContentView(R.layout.activity_body_mass_index)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        init()
+    }
+    private fun init() {
+        toolbarBodyMassIndex = binding.toolbarBodyMassIndex
+        setSupportActionBar(toolbarBodyMassIndex)
+        title = getString(R.string.toolbar_title)
+        toolbarBodyMassIndex.subtitle = getString(R.string.toolbar_subtitle)
+        toolbarBodyMassIndex.setLogo(R.drawable.toolbar_icon)
+
+        textViewBodyMassIndexTV = binding.textViewBodyMassIndexTV
+        imageViewIV = binding.imageViewIV
+        textViewRecommendationsTV = binding.textViewRecommendationsTV
+
+        var height = intent.getStringExtra("height")
+        val weight = intent.getStringExtra("weight")
+        if (height != null && weight != null){
+            val bodyMassIndex = BodyMassIndex (height,weight)
+            textViewBodyMassIndexTV.text = bodyMassIndex.indexPrint()
+            textViewRecommendationsTV.text = bodyMassIndex.recommendations()
+            bodyMassIndex.imageIndex(imageViewIV)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_bode_mass_index, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    @SuppressLint("SetTextI18n", "ShowToast")
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.exitMenuMain -> {
+                Toast.makeText(
+                    applicationContext,
+                    getString(R.string.toast_exitBodyMassIndex),
+                    Toast.LENGTH_LONG
+                ).show()
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+}
---
+++ app/src/main/java/com/example/samojlov_av_homework_module_10_number_6/MainActivity.kt
@@ -0,0 +1,90 @@
+package com.example.samojlov_av_homework_module_10_number_6
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.samojlov_av_homework_module_10_number_6.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var toolbarMain: androidx.appcompat.widget.Toolbar
+    private lateinit var editTextHeightET: EditText
+    private lateinit var editTextWeightET: EditText
+    private lateinit var  calculateLaunchBTN: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+//        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        init()
+    }
+    private fun init() {
+        toolbarMain = binding.toolbarMain
+        setSupportActionBar(toolbarMain)
+        title = getString(R.string.toolbar_title)
+        toolbarMain.subtitle = getString(R.string.toolbar_subtitle)
+        toolbarMain.setLogo(R.drawable.toolbar_icon)
+
+        editTextHeightET = binding.editTextHeightET
+        editTextWeightET = binding.editTextWeightET
+        calculateLaunchBTN = binding.calculateLaunchBTN
+
+        calculateLaunchBTN.setOnClickListener {
+            if (editTextHeightET.text.isEmpty() || editTextWeightET.text.isEmpty())
+                return@setOnClickListener
+            val intent = Intent(this, BodyMassIndexActivity::class.java)
+            intent.putExtra("height", editTextHeightET.text.toString())
+            intent.putExtra("weight", editTextWeightET.text.toString())
+            startActivity(intent)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    @SuppressLint("SetTextI18n", "ShowToast")
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.resetMenuMain -> {
+                editTextHeightET.text.clear()
+                editTextWeightET.text.clear()
+                Toast.makeText(
+                    applicationContext,
+                    getString(R.string.toast_reset),
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+
+            R.id.exitMenuMain -> {
+                Toast.makeText(
+                    applicationContext,
+                    getString(R.string.toast_exit),
+                    Toast.LENGTH_LONG
+                ).show()
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+}
